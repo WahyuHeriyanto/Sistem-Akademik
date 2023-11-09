9b28fa1ab886d287e2097ca5fbfc91c8c01bc8a3
WahyuHeriyanto <110623892+WahyuHeriyanto@users.noreply.github.com>
2023-11-09 23:19:32 +0700

    Add files via upload

---
+++ native/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistem Akademik</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link rel='stylesheet' href="style.css">
+
+</head>
+
+<body background="login.jpg">
+
+<div class="container">
+  
+
+  <form>
+      <img class="mt-2" src="uns.png" alt="" width="120" height="140">
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <div class="form-check text-start my-3">
+        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+          Remember me
+        </label>
+      </div>
+      <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-body-secondary">&copy; Web Akademik 2023</p>
+  </form>
+  <!--dasboard.html-->
+  Sistem backend belum dibikin, sementara <a href="dashboard/dashboard.php">Klik disini</a>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ native/krs/cetak.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KARTU RENCANA STUDI</title>
+</head>
+<body>
+ 
+	<center>
+        <h2>KARTU RENCANA STUDI</h2>
+		<h2>(KHS)</h2>
+ 
+	</center>
+
+
+ 
+	<table border="1" style="width: 100%">
+		<tr>
+			<th width="1%">No</th>
+            <th>Kode Mata Kuliah</th>
+			<th>Mata Kuliah</th>
+			<th>Kelas</th>
+			<th width="5%">SKS</th>
+		</tr>
+        <tr>
+      <td>1</td>
+      <td>EE0501-19</td>
+      <td>JARINGAN KOMUNIKASI DATA</td>
+      <td>B</td>
+      <td>2</td>
+
+	
+	</table>
+ 
+	<script>
+		window.print();
+	</script>
+ 
+</body>
+</html>
